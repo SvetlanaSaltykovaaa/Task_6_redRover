6fface719febf7c3bc9523d15fd129cad15535b3
Sveta Saltykova <libanonair@gmail.com>
2024-09-12 10:37:41 +0300

    task 7.1 done

---
+++ src/Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+
+
+    String name;
+    int baseSalary;
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
